038781d5a40d77911a15e355c0234362f9bc9b96
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-02-25 16:58:26 +0800

    remove useless comment

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
+local function ddf()
+        core.display_chat_message("You died.")
+        local formspec = "size[11,5.5]bgcolor[#320000b4;true]" ..
+                "label[4.85,1.35;" .. fgettext("You died") ..
+                "]button_exit[4,3;3,0.5;btn_respawn;".. fgettext("Respawn") .."]"
+        core.show_formspec("bultin:death", formspec)
+end
 if minetest.get_server_info().address == "ctf.rubenwardy.com" then
-	minetest.register_on_death(function()
-		minetest.show_formspec("ctf_util:blank", "formspec_version[1]size[11,5.5]bgcolor[#320000b4;true]"..
-                "label[4.85,1.35;You died]"..
-                "button_exit[4,3;3,0.5;fake_btn_respawn;Toggled The Timer]")
-		minetest.send_respawn()
+	table.remove(core.registered_on_death,1)
+	core.register_on_death(function()
+		core.display_chat_message("You died. Requesting respawn.")
+		core.send_respawn()
 	end)
 end
